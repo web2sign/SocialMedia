b874658c5fcb7fe49e69f709c4efb114a3f8a806
Roy Vincent Niepes <web2sign@gmail.com>
2018-07-02 14:58:14 +0800

    database cleanup

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -13,7 +13,6 @@
       </h1>
       <ol class="breadcrumb">
         <li><a href="{{ url('/') }}"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
         <li class="active">404 error</li>
       </ol>
     </section>
